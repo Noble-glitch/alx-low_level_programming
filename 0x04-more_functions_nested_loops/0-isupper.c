105d2b175904b6dc2c51191532e336f9d18357c1
Noble-glitch <adewoleoyeleye2@gmail.com>
2023-03-24 14:52:43 -0700

    main program dot h

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,14 @@
  *       *
  *         * Return: 1 for upper letter or 0 for any else
  *           */
-int _isupper(int x)
+int _isupper(int c)
 {
-		if (x >= 65 && x <= 90)
-				{
-							return (1);
-								}
-
-			return (0);
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
